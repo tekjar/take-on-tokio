a8366c75eb3ebfde3a9914fd5be0ed3700f4edcd
tekjar <k.teza1@gmail.com>
2017-07-29 23:53:55 +0530

    add andthen playground example

---
+++ README.md
@@ -0,0 +1,5 @@
+# TRY FUTURES AND TOKIO
+
+### example
+ 
+ [andthen.rs](https://play.rust-lang.org/?gist=2ca1351ebd87f49e132d9c10a011ed30&version=stable)
--- futures/basics/andthen/src/main.rs
+++ futures/basics/andthen/src/main.rs
@@ -8,16 +8,26 @@ fn main() {
     let mut reactor = Core::new().unwrap();
     let f1 = ok::<i32, i32>(1);
 
+    // AndThen Combinator:
+    // -------------------
+    // fn and_then<F, B>(self, f: F) -> AndThen<Self, B, F> 
+    //    where
+    //    F: FnOnce(Self::Item) -> B,          * F takes self's item when it is successful and should produce
+    //    B: IntoFuture<Error = Self::Error>,  * a (Into)Future B whose error type is same as that of Self's.
+    //    Self: Sized,                         * Self should also be Sized
+    
+    
+    
     // NOTES: 
-    // 1. and_then executes the closures only when future is successful
-    // 2. closure should return an IntoFuture (Result implements it) whose error is same as that of f1's
+    // 1. and_then's closure is executed only when the future is successful
+    // 2. closure should return an IntoFuture (Result implements it) and it's error type should be same as f1's error type
     // 3. item type of intofuture return from closure can be anything
     let f = f1.and_then(|v| {
         println!("{:?}", v);
-        
-        // Ok::<(), f32>(()) // doesn't work (2)
+        // Ok::<(), f32>(())    // doesn't work (2)
         // Ok::<i32, i32>(100)  // works (2)
-        Ok(true) // works (2) (3)
+        // Err(100)             // works (2)
+        // Ok(true)             // works (2) (3)
     });
 
     assert_eq!(Ok(true), reactor.run(f));
