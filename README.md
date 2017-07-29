c7236dc2242636eedb5ade1f77554fee7fa18728
tekjar <k.teza1@gmail.com>
2017-07-30 00:40:24 +0530

    adding map playground example

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # FUTURES AND TOKIO BY EXAMPLES
 
- ##BASICS
+ ### BASICS
  ---------
 
- [andthen.rs](https://play.rust-lang.org/?gist=4992e9a7195f962b91a0386767cf2436&version=stable)
- [orelse.rs]()
+[andthen.rs](https://play.rust-lang.org/?gist=dd221b3e18d5c415622218d29cac352b&version=stable)
+  
+[orelse.rs](https://play.rust-lang.org/?gist=254a37c38215ebeb2f92e343d70ad617&version=stable)
+   
+[map.rs](https://play.rust-lang.org/?gist=15bcaa3f3476a3f7225876ec2de510fd&version=stable)
--- futures/basics/map/src/main.rs
+++ futures/basics/map/src/main.rs
@@ -7,20 +7,29 @@ use tokio_core::reactor::Core;
 fn main() {
     let mut reactor = Core::new().unwrap();
     let f1 = ok::<i32, i32>(1);
+    
+    // Map Combinator:
+    // ---------------
+    //
+    // fn map<F, U>(self, f: F) -> Map<Self, F> 
+    // where                           * F get's executed when Self resolves to an error.
+    //   F: FnOnce(Self::Item) -> U,   * No restrictions on return type. map() will envolope the result with 'Ok'. 
+    //                                   So unlike 'andthen' and 'orelse', map will always resolves to 'Ok' when Self in succesful
+    //   Self: Sized,                  
 
     // NOTES: 
-    // 1. map executes the closures only when future results ok or else passes the future along unaltered
+    // 1. map's closure get's executed only when self results ok or else passes the future along unaltered
     // 2. doesn't care about item, error type matching b/w f1 & closure because of below point 
     //    ( but Map item type will be closure return type & error type will be same as that of f1)
     // 3. closure return value doesn't have to be intofuture
     //    ( but Map will take care of converting closure return type to Result (which is an intofuture) by
-    //      With Ok::<_, f1's error type>(closure return) /)
+    //      with Ok::<_, f1's error type>(closure return) /)
     let f = f1.map(|v| {
         println!("{:?}", v);
         true // (2), (3)
     });
 
-    assert_eq!(Ok(true), reactor.run(f)); // Map poll will wrap 'true' to Ok(true)
+    assert_eq!(Ok(true), reactor.run(f)); // Map's poll will wrap 'true' to Ok(true)
 
     let f1 = ok::<i32, i32>(1);
     let f = f1.map(|v| {
@@ -29,6 +38,14 @@ fn main() {
     });
 
     assert_eq!(Ok(Ok(true)), reactor.run(f));
+    
+    let f1 = ok::<i32, i32>(1);
+    let f = f1.map(|v| {
+        println!("{:?}", v);
+        Err::<(), _>(-1)
+    });
+
+    assert_eq!(Ok(Err(-1)), reactor.run(f));
 
     let f1 = err::<i32, i32>(-1);
     let f = f1.map(|v| {
