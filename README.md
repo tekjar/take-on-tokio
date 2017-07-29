ed44016d004ef4773166b5e6e75e30d8eb0223ef
tekjar <k.teza1@gmail.com>
2017-07-30 03:53:34 +0530

    adding then playground example

--- README.md
+++ README.md
@@ -10,3 +10,5 @@
 [map.rs](https://play.rust-lang.org/?gist=684ec52df09df0b28a3a83cf7a134a8d&version=stable)
   
 [maperr.rs](https://play.rust-lang.org/?gist=1e7dedb2dde52d318109c5a19998c654&version=stable)
+  
+[then.rs](https://play.rust-lang.org/?gist=97d1df505de08451e3a8372345465991&version=stable)
--- futures/basics/then/src/main.rs
+++ futures/basics/then/src/main.rs
@@ -8,18 +8,27 @@ fn main() {
     let mut reactor = Core::new().unwrap();
     let f1 = ok::<i32, i32>(1);
 
+    // Then Combinator
+    // ---------------
+    //
+    // fn then<F, B>(self, f: F) -> Then<Self, B, F> 
+    // where                                                * F get's executed when Self(Future) resolves to error (or) success
+    //   F: FnOnce(Result<Self::Item, Self::Error>) -> B,   * No restrictions on return type of the closure
+    //   B: IntoFuture,                                     * but closure return should be IntoFuture
+    //   Self: Sized,
+
     // NOTES: 
-    // 1. then executes the closures regardless future's results. closure arg take full future unlike other
-    //    combinators which take completed values (or skip altogether)
+    // 1. Then executes the closure regardless of future's result. Unlike other
+    //    combinators which gets executed based on future's success/failure, Then always executes the closure
     // 2. doesn't care about item, error type matching b/w f1 & closure
     //    ( but 'Then's item, error type will be closure returned intofuture's item, error type)
-    // 3. closure should be an intofuture
+    // 3. closure return should be an intofuture
     let f = f1.then(|v| {
         println!("{:?}", v);
         Ok::<bool, i64>(true)
     });
 
-    assert_eq!(Ok(true), reactor.run(f)); // Map poll will wrap 'true' to Ok(true)
+    assert_eq!(Ok(true), reactor.run(f)); // Then poll will return Ok(true)
 
     let f1 = err::<i32, i32>(-1);
     let f = f1.then(|v| {
